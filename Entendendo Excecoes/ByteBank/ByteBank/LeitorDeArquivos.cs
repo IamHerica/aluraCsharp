b005648348ef2c07737c93f2489e47c61500059c
Iamherica <hericacadoni@gmail.com>
2021-07-05 11:04:13 -0300

    trabalhando com o using para ler arquivos

---
+++ Entendendo Excecoes/ByteBank/ByteBank/LeitorDeArquivos.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Text;
+
+namespace ByteBank
+{
+    class LeitorDeArquivos : IDisposable //assinando o contrato com a interface 
+    {
+
+        public string Arquivo { get; }
+
+        public LeitorDeArquivos(string arquivo)
+        {
+            Arquivo = arquivo;
+
+           // throw new FileNotFoundException();
+            
+            Console.WriteLine("Abrindo arquivo: " + arquivo);
+        }
+        public string LerProximaLinha()
+        {
+            Console.WriteLine("Lendo linha...");
+
+
+            throw new IOException();
+
+
+            return "Linha do arquivo";
+        }
+
+        //metodo que tem a funcao de liberar os recursos, fechar o arquivo
+        public void Dispose()
+        {
+            Console.WriteLine("Fechando arquivo");
+        }
+    }
+}
--- Entendendo Excecoes/ByteBank/ByteBank/Program.cs
+++ Entendendo Excecoes/ByteBank/ByteBank/Program.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.IO;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -12,6 +13,58 @@ namespace ByteBank
         {
             try
             {
+                CarregarContas();
+            }
+            catch (Exception)
+            {
+                Console.WriteLine("CATCH NO MAIN");
+            }
+
+            Console.WriteLine("Execução finalizada.");
+
+        }
+
+        private static void CarregarContas()
+        {
+            //using testa se é nula, é como se fosse todo o try e finally. além disso, quando terminar ele já chama o dispose(fechar arquivo)
+            using (LeitorDeArquivos leitor = new LeitorDeArquivos("teste.txt")) //deve implementar a interface IDisposable
+            {
+                leitor.LerProximaLinha();
+            }
+
+
+
+
+
+
+            //LeitorDeArquivos leitor = new LeitorDeArquivos("contas.txt");
+
+            //try
+            //{
+
+            //    leitor.LerProximaLinha();
+            //    leitor.LerProximaLinha();
+
+            //}
+            //catch (IOException)
+            //{
+
+            //    Console.WriteLine("Exceção do tipo IOException capturada e tratada");
+            //} finally
+            //{
+
+
+
+            //    leitor.Fechar();
+            //}
+
+        }
+
+
+        private static void TestaInnerException()
+        {
+            try
+            {
                 ContaCorrente conta = new ContaCorrente(98, 74640);
 
                 ContaCorrente conta2 = new ContaCorrente(78, 986);
@@ -19,20 +72,26 @@ namespace ByteBank
                 //conta.Transferir(1000, conta2);
                 conta.Sacar(80000);
 
-            } catch (OperacaoFinanceiraException e) //tanto transferencia como saque
+            }
+            catch (OperacaoFinanceiraException e) //tanto transferencia como saque
             {
                 Console.WriteLine(e.Message);
                 Console.WriteLine(e.StackTrace);
 
 
 
-               //acessando para verificar o que acontece no sistema
-               // Console.WriteLine("\n\n\n\nInformações da INNER EXCEPTION (exceção interna):");
-               // Console.WriteLine("\n\n" + e.InnerException.Message);
-               // Console.WriteLine("\n" + e.InnerException.StackTrace);
+                //acessando para verificar o que acontece no sistema
+                // Console.WriteLine("\n\n\n\nInformações da INNER EXCEPTION (exceção interna):");
+                // Console.WriteLine("\n\n" + e.InnerException.Message);
+                // Console.WriteLine("\n" + e.InnerException.StackTrace);
             }
-
         }
+
+
+
+
+
+
 
         private static void Metodo()
         {
